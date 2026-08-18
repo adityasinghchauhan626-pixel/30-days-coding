ccae774b7630be866ff70b972adc345e74455d51
Aditya Singh Chouhan <adityasinghchauhan626@gmail.com>
2026-08-18 21:38:35 +0530

    Add files via upload

---
+++ Day-4/basic.java
@@ -0,0 +1,46 @@
+class detail{
+
+    private String name;
+    private int age;
+
+
+    public void setname(String name){
+
+        this.name = name;
+
+    }
+
+    public void setage(int age){
+
+        this.age = age;
+
+    }
+
+    public String getname(){
+
+        return name;
+    }
+
+    public int  getage(){
+
+        return age;
+    }
+}
+
+class basic{
+
+    public static void main(String[] args) {
+
+
+        detail s1 = new detail();
+        s1.setname("Aditya");
+        s1.setage(10);
+
+
+        s1.getname();
+        s1.getage();
+
+        System.out.println(s1.getname());
+    
+    }
+}
