00b1af497d813173cd6bf953bf95cc5d20b9ea22
Aditya Singh Chouhan <adityasinghchauhan626@gmail.com>
2026-08-18 22:36:35 +0530

    Add files via upload

--- Day-4/student.java
+++ Day-4/student.java
@@ -1,43 +1,52 @@
-class detail{
+import java.util.Scanner;
+class s{
 
-    String name;
-    int age;
-    int rollno;
+    private String name;
+    private int marks;
 
-    detail(){
+    public void setname(String name){
+        this.name = name;
+    }
 
-        System.out.println("constructor 1");
-        this("Aditya",20);
-
+    public void setmarks(int marks){
+        this.marks= marks;
 
     }
 
-    detail(String name , int age ){
-
-        this.name = name;
-        this.age = age;
-
-        System.out.println("constructor 2 ");
-
-        this("Aryan ",21,15);
-
+    public String getname(){
+        return name;
     }
-
-    detail(String name , int age , int rollno){
-        this.name = name;
-        this.age = age;
-        this.rollno = rollno;
-
-        System.out.println("constructor 3");
-
-
+    public int getmarks(){
+        return marks;
     }
 }
-
 class student{
     public static void main(String[] args) {
-        detail s1 = new detail();
+        Scanner scn = new Scanner(System.in);
+        s s1 = new s();
+        System.out.println("Enter name :");
 
-        System.out.println(s1.name);
+        String name = scn.nextLine();
+
+        System.out.println("Enter the marks:");
+        int marks = scn.nextInt();
+
+        s1.setname(name);
+        s1.setmarks(marks);
+
+        if(s1.getmarks()>100 || s1.getmarks()<0){
+            System.out.println("Invalid marks:");
+
+        }
+
+        else{
+            System.out.println("Name :" + s1.getname());
+            System.out.println("Marks :" + s1.getmarks());
+        }
+        scn.close();
+    
     }
+    
+
 }
+
