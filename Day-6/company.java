6335dbaa1c9d466cc48bbc97cd217f447e84acb2
Aditya Singh Chouhan <adityasinghchauhan626@gmail.com>
2026-08-20 23:01:57 +0530

    Add files via upload

--- Day-6/company.java
+++ Day-6/company.java
@@ -1,12 +1,20 @@
-class employee{
+class person{
     String name;
+    int age;
+
+    void displayperson(){
+        System.out.println("Name of the person: " +name);
+        System.out.println("Age of the person" +age);
+    }
+}
+
+class employee extends person{
     int employeeid;
-    int salary;
+    double salary;
 
     void displayemployee(){
-        System.out.println("Name of employee is "+name);
-        System.out.println("Employee id is "+employeeid);
-        System.out.println("Salary of employee is "+salary);
+        System.out.println("Employee id is " +employeeid);
+        System.out.println("Salary is " +salary);
     }
 }
 
@@ -14,21 +22,21 @@ class manager extends employee{
     String department;
 
     void displaymanager(){
-
-        System.out.println("Department "+department);
-
+        System.out.println("Department is" +department);
     }
 }
 class company{
     public static void main(String[] args) {
         manager s1 = new manager();
-
         s1.name = "Aditya";
-        s1.employeeid= 250133;
+        s1.age = 20;
+        s1.employeeid = 250133;
         s1.salary = 56000;
         s1.department = "Technical";
 
+        s1.displayperson();
         s1.displayemployee();
         s1.displaymanager();
+        
     }
 }
