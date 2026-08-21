f7fb6dabf6054d03855f2555a85acb308222df10
Aditya Singh Chouhan <adityasinghchauhan626@gmail.com>
2026-08-21 09:32:12 +0530

    Add files via upload

---
+++ Day-7/vechicalsss.java
@@ -0,0 +1,16 @@
+class vechical{
+    static void display(){
+        System.out.println("This is a vechile");
+    }
+}
+class car{
+    static void display(){
+        System.out.println("This is a car");
+    }
+}
+class vechicalsss{
+    public static void main(String[] args) {
+        vechical.display();
+        car.display();
+    }
+}
