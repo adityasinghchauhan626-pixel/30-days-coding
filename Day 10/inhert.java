ad0ede0a2ef737e9cafc6babe0a31665948d26b1
Aditya Singh Chouhan <adityasinghchauhan626@gmail.com>
2026-08-25 21:49:22 +0530

    Add files via upload

---
+++ Day 10/inhert.java
@@ -0,0 +1,97 @@
+// class name{
+
+//     String name ="Aditya";
+
+
+// }
+
+// class student extends name{
+
+//     String name ="aman";
+
+//     void display(){
+//         System.out.println(this.name);
+//         System.out.println(super.name);
+
+//     }
+// }
+// class inhert{
+
+//     public static void main(String[] args) {
+//         student s1 = new student();
+//         s1.display();
+
+
+
+
+//     }
+// }
+// import java.util.Scanner;
+// class account{
+
+//     int accountno;
+//     int balance;
+
+
+// }
+
+// class child extends account{
+//     String accountype;
+//     int intrest;
+
+//     void display(){
+//         Scanner scn = new Scanner(System.in);
+
+//         System.out.println("Enter the account no:");
+//         accountno = scn.nextInt();
+
+//         System.out.println("Enter the balance :");
+
+//         balance = scn.nextInt();
+
+//         System.out.println("account type :");
+
+//         accountype =scn.nextLine();
+
+//         System.out.println("intrest:");
+
+//         intrest = scn.nextInt();
+//         scn.close();
+
+//         System.out.println(accountno);
+//         System.out.println(balance);
+//         System.out.println(accountype);
+//         System.out.println(intrest);
+
+
+
+//     }
+
+
+
+
+// }
+
+// class inhert{
+//     public static void main(String[] args) {
+//         child c = new child();
+//         c.display();
+//     }
+// }
+
+
+ class inhert{
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        //System.out.println(100/2);
+        System.out.println(100/0);
+        
+
+
+    }
+ }
+
+
