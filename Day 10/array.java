7a6c542478c90b3fd5f55d7fc7d41512d247d73f
Aditya Singh Chouhan <adityasinghchauhan626@gmail.com>
2026-08-30 21:23:39 +0530

    Add files via upload

---
+++ Day 10/array.java
@@ -0,0 +1,33 @@
+//  package abhi;
+// import java.util.Scanner;
+// import java.util.Arrays;
+
+// public class Demoarray2 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter the size of the array: ");
+//         int n = sc.nextInt();
+
+//         int[] arr = new int[n];
+
+//         System.out.println("Enter " + n + " elements:");
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+
+//         System.out.println("Array elements are:");
+//         for (int i = 0; i < n; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//         Arrays.sort(arr);
+
+//         System.out.println("\n"+"Sorted array:");
+//         for (int num : arr) {
+//             System.out.print(num + " ");
+//         }
+
+
+//         sc.close();
+//     }
+// }
