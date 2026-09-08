1d995000dc1d51d91a961db177e9392ef5701308
Aditya Singh Chouhan <adityasinghchauhan626@gmail.com>
2026-09-08 22:52:17 +0530

    Add files via upload

---
+++ Day 11/pattern3.java
@@ -0,0 +1,13 @@
+public class pattern3{
+    public static void main(String[] args) {
+        int n =1;
+        for(int i=5;i>=n;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            
+
+            }
+            System.out.println();
+        }
+    }
+}
